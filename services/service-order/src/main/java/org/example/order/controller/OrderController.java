16d8a33505ca492a3a299c70d0a946d92749a045
QiuLin-hao <210554114@qq.com>
2025-02-14 10:29:15 +0800

    feat(services): 添加 Nacos配置中心支持
    - 在 service-order 和 service-product 的 application.properties 文件中添加 Nacos 相关配置
    - 修改 OrderController，支持从 Nacos 配置中心获取配置信息
    - 在 services/pom.xml 中添加 spring-cloud-starter-alibaba-nacos-config 依赖

--- services/service-order/src/main/java/org/example/order/controller/OrderController.java
+++ services/service-order/src/main/java/org/example/order/controller/OrderController.java
@@ -4,15 +4,30 @@ package org.example.order.controller;
 import org.example.order.bean.Order;
 import org.example.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class OrderController {
 
     @Autowired
     OrderService orderService;
+
+    @Value("${order.timeout}")
+    String orderTimeout;
+
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @GetMapping("/config")
+    public String getConfig() {
+        return "order.timeout=" + orderTimeout + "； " +
+                "order.auto-confirm=" + orderAutoConfirm;
+    }
 
     @GetMapping("/create")
     public Order createOder(@RequestParam("productId") Long productId,
