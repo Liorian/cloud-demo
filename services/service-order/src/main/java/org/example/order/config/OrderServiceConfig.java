fe89c0863a5033f5c2bcbcdf1c4bae3bdc5db773
QiuLin-hao <210554114@qq.com>
2025-03-06 22:48:04 +0800

    feat(order): 添加重试机制和 Feign 日志
    - 添加 Retryer bean 以启用重试机制
    - 添加 Logger.Level bean 以启用 Feign 日志

--- services/service-order/src/main/java/org/example/order/config/OrderServiceConfig.java
+++ services/service-order/src/main/java/org/example/order/config/OrderServiceConfig.java
@@ -1,5 +1,7 @@
 package org.example.order.config;
 
+import feign.Logger;
+import feign.Retryer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,27 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OrderServiceConfig {
+
+    /**
+     * 开启重试机制
+     *
+     * @return Retryer
+     */
+    @Bean
+    Retryer retryer() {
+        return new Retryer.Default();
+    }
+
+
+    /**
+     * 开启Feign日志
+     *
+     * @return Logger.Level
+     */
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 
     @LoadBalanced // 注解式负载均衡
     @Bean
